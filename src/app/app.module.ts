4631b57fa1f91c3e5c360781824e8a57d8f9f753
Ahmed <101567711+AhmedBSF@users.noreply.github.com>
2023-08-23 23:00:36 +0300

    initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductItemDetailsComponent } from './components/product-item-details/product-item-details.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { CardComponent } from './components/card/card.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    ProductItemDetailsComponent,
+    ProductItemDetailComponent,
+    ConfirmationComponent,
+    CardComponent,
+    HeaderComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
