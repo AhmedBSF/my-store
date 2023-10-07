dcd717c0b0cedeb9c61533f7c230a65ea95fa576
Ahmed <101567711+AhmedBSF@users.noreply.github.com>
2023-10-07 21:03:45 +0300

    feat: cart logic

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/models/order';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cartProducts: Order[] = [];
+  cartEmptyMessage: string = 'Your cart is currently empty!';
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartProducts = this.cartService.getCart();
+  }
 }
